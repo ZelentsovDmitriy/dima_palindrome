ea5770f5af558ce9d6007bfa9bd22934d32ca628
root <root@vm-fa38908a.na4u.ru>
2023-03-09 08:36:16 +0000

    Initialize repository

---
+++ lib/dima_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dima_palindrome/version"
+
+module DimaPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dima_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DimaPalindrome
+  VERSION = "0.1.0"
+end
